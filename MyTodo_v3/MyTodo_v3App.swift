daaf72a7725cf71b00a4513994ac78a80e473c54
ogiwelcome <ogi.fordevelopment@gmail.com>
2021-10-02 23:21:54 +0900

    Initial Commit

---
+++ MyTodo_v3/MyTodo_v3App.swift
@@ -0,0 +1,17 @@
+//
+//  MyTodo_v3App.swift
+//  MyTodo_v3
+//
+//  Created by 荻野真浩 on 2021/10/02.
+//
+
+import SwiftUI
+
+@main
+struct MyTodo_v3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
